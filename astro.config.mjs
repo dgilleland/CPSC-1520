1b64aca92c0b24590ba60d1d890c1870920b3945
Dan Gilleland <dagilleland@shaw.ca>
2025-09-26 22:17:36 -0600

    Merge astro.config.mjs - Fixing indentation
    Stashed changes had cleaned-up indentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,63 +11,63 @@ export default defineConfig({
     site: 'https://dgilleland.github.io',
     base: '/CPSC-1520',
     integrations: [starlight({
-			title: 'CPSC-1520-DG',
-			editLink: {
-			  baseUrl: 'https://github.com/dgilleland/CPSC-1520/edit/main/',
-			},
-			customCss: [
-			// Path to your Tailwind base styles:
-			'./src/tailwind.css',
-			],
-			social: {
-				github: 'https://github.com/dgilleland/CPSC-1520',
-			},
-			sidebar: [
-				{
-					label: 'About',
-					collapsed: true,
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Start Here', slug: 'about/start-here' },
-						{ label: 'Change Log', slug: 'about/change-log' },
-						{ label: 'The Roadmap', slug: 'about/roadmap' },
-						// An external link to the NASA website opening in a new tab.
-						{
-							label: 'JavaScript Reference',
-							link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-							attrs: { target: '_blank', style: 'font-style: italic' },
-							badge: { text: 'MDN', variant: 'note' }
-						},
-						{ label: 'Lab Setup', slug: 'about/lab-setup' },
-						{ label: 'Lab Updates', slug: 'about/lab-updates' },
-						{ label: 'Bonus Resources', slug: 'about/bonus' },
-					],
-				},
-				{
-					label: 'Tutorials',
-					autogenerate: { directory: 'tutorials' },
-				},
-				{
-					label: 'How-To Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Explanations',
-					autogenerate: { directory: 'explanations' },
-					collapsed: false,
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-					collapsed: false,
-				},
+        title: 'CPSC-1520-DG',
+        editLink: {
+          baseUrl: 'https://github.com/dgilleland/CPSC-1520/edit/main/',
+        },
+        customCss: [
+        // Path to your Tailwind base styles:
+        './src/tailwind.css',
+        ],
+        social: {
+            github: 'https://github.com/dgilleland/CPSC-1520',
+        },
+        sidebar: [
+            {
+                label: 'About',
+                collapsed: true,
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Start Here', slug: 'about/start-here' },
+                    { label: 'Change Log', slug: 'about/change-log' },
+                    { label: 'The Roadmap', slug: 'about/roadmap' },
+                    // An external link to the NASA website opening in a new tab.
+                    {
+                        label: 'JavaScript Reference',
+                        link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+                        attrs: { target: '_blank', style: 'font-style: italic' },
+                        badge: { text: 'MDN', variant: 'note' }
+                    },
+                    { label: 'Lab Setup', slug: 'about/lab-setup' },
+                    { label: 'Lab Updates', slug: 'about/lab-updates' },
+                    { label: 'Bonus Resources', slug: 'about/bonus' },
+                ],
+            },
+            {
+                label: 'Tutorials',
+                autogenerate: { directory: 'tutorials' },
+            },
+            {
+                label: 'How-To Guides',
+                autogenerate: { directory: 'guides' },
+            },
+            {
+                label: 'Explanations',
+                autogenerate: { directory: 'explanations' },
+                collapsed: false,
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+                collapsed: false,
+            },
             {
                 label: 'Demos',
                 autogenerate: { directory: 'demos' },
             },
-			],
+        ],
 		}), tailwind({
-			// Disable the default base styles:
-			applyBaseStyles: false,
+        // Disable the default base styles:
+        applyBaseStyles: false,
 		}), svelte()],
 });
